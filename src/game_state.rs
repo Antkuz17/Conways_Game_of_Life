b0d81574be6b80106421a8dec84ae6b48e0b499d
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2026-03-02 11:44:09 -0500

    feat: Game stat struct largely complete with update function

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,3 +1,5 @@
+use crate::grid::Grid;
+
 pub struct GameState {
     CurrentGrid: Grid,
     NextGrid: Grid,
@@ -8,10 +10,10 @@ pub struct GameState {
 impl GameState {
     pub fn new(width: usize, height: usize) -> Self{
         Self {
-            CurrentGrid = Grid::new(width, height),
-            NextGrid = Grid::new(width, height),
-            CurrentGeneration = 0,
-            CurrentPopulation = 0,
+            CurrentGrid: Grid::new(width, height),
+            NextGrid: Grid::new(width, height),
+            CurrentGeneration: 0,
+            CurrentPopulation: 0,
         }
     }
 
@@ -36,5 +38,50 @@ impl GameState {
             panic!("Generation number cannot be negative");
         }
         self.CurrentGeneration = generation;
+    }
+    /*
+    Central logic function for the entire grid   
+    The function will update the grid for the next generation
+    */
+    pub fn update(&mut self) {
+        for row in 0..self.CurrentGrid.get_height()
+        {
+            for col in 0..self.CurrentGrid.get_width() 
+            {
+                // Get the number of live neighbors and the current cell status for the current cell
+                let live_neighbors = self.CurrentGrid.get_num_live(row, col);
+                let current_cell = self.CurrentGrid.get_cell(row, col).unwrap_or(false);
+
+                // Apply the rules of the game to determine the status of the cell in the next generation
+
+                // If the current cell is alive
+                if (current_cell)
+                {   
+                    // If the cell has 2-3 living partners it lives
+                    if live_neighbors > 1 && live_neighbors < 4 {
+                        NextGrid.set_current_population(get)
+                        continue;
+                    }
+                    // Anything else and it dies
+                    else{
+                        NextGrid.set_cell(row, col, false)
+                    }
+                }
+
+                // If the current cell is dead
+                else
+                {
+                    if live_neighbors == 3 {
+                        NextGrid.set_cell(row, col, true);
+                    }
+                }
+
+            
+            }
+        }
+
+        // Updating the current generation counter
+        set_current_generation(get_generation() + 1);
+
     }
 }
