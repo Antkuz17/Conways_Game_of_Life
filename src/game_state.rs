4fb800ed74a1b31ae18e4370e533aa90d1448997
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2026-03-02 10:42:44 -0500

    feat: simple game state getters and setters created

--- src/game_state.rs
+++ src/game_state.rs
@@ -3,5 +3,38 @@ pub struct GameState {
     NextGrid: Grid,
     CurrentGeneration: usize,
     CurrentPopulation: usize,
-    
+}
+
+impl GameState {
+    pub fn new(width: usize, height: usize) -> Self{
+        Self {
+            CurrentGrid = Grid::new(width, height),
+            NextGrid = Grid::new(width, height),
+            CurrentGeneration = 0,
+            CurrentPopulation = 0,
+        }
+    }
+
+    // Get generation number
+    pub fn get_generation(&self) -> usize{
+        self.CurrentGeneration
+    }
+
+    // Get current population
+    pub fn get_population(&self) -> usize {
+        self.CurrentPopulation
+    }
+
+    // Get current population on the grid
+    pub fn set_current_population(&mut self, population: usize) {
+        self.CurrentPopulation = population;
+    }
+
+    // Set current generation number
+    pub fn set_current_generation(&mut self, generation: usize) {
+        if generation < 0 {
+            panic!("Generation number cannot be negative");
+        }
+        self.CurrentGeneration = generation;
+    }
 }
